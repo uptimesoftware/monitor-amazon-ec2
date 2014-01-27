0e2cd0c7ed823d6f7bd00d47f1e2f26fa78ce79b
Abdullah Salim <salim.abdullah@gmail.com>
2014-01-27 10:53:33 -0500

    -- added availability metric

--- pkg/files/scripts/Amazon_EC2_Monitor/Amazon_EC2_Monitor.py
+++ pkg/files/scripts/Amazon_EC2_Monitor/Amazon_EC2_Monitor.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 
-import os
 import sys
+import os
 import boto
-import itertools
 import datetime
 
 from boto.exception import BotoServerError
 
 AWS_ACCESS_KEY = os.environ.get('UPTIME_AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.environ.get('UPTIME_AWS_SECRET_KEY')
+
+# EC2 statuses
+EC2_STATUS_PENDING = "pending"
+EC2_STATUS_RUNNING = "running"
+EC2_STATUS_SHUTTING_DOWN = "shutting-down"
+EC2_STATUS_TERMINATED = "terminated"
+EC2_STATUS_STOPPING = "stopping"
+EC2_STATUS_STOPPED = "stopped"
 
 class Error(Exception):
     pass
@@ -22,29 +29,26 @@ def main(argv):
 		instances = [i for r in reservations for i in r.instances]
 
 		c = boto.connect_cloudwatch(AWS_ACCESS_KEY, AWS_SECRET_KEY)
-		end   = datetime.datetime.now()
+		end = datetime.datetime.now()
 		start = end - datetime.timedelta(hours=1)
 
 		metrics = ['CPUUtilization', 'DiskReadBytes', 'DiskReadOps', 'DiskWriteBytes', 'DiskWriteOps', 'NetworkIn', 'NetworkOut']
-
+		
 		for instance in instances:
-			for j in range(len(metrics)):
-				stats = c.get_metric_statistics(
-    				60, 
-					start, 
-					end, 
-					metrics[j], 
-					'AWS/EC2', 
-					'Average', 
-					{'InstanceId' : instance.id}
-				)
-
-				if stats:
+			print instance.id + ".Availability " + instance.state
+			if instance.state == EC2_STATUS_RUNNING:	# pull stats for running instances only
+				for j in range(len(metrics)):
+					stats = c.get_metric_statistics(
+						60, 
+						start, 
+						end, 
+						metrics[j], 
+						'AWS/EC2', 
+						'Average', 
+						{'InstanceId' : instance.id}
+					)
 					print instance.id + "." + metrics[j],
-					print stats[len(stats)-1]['Average']
-				else:
-					print instance.id
-					break
+					print stats[len(stats)-1]['Average']	# get last reading
 
 	except BotoServerError as serverErr:
 		print json.dumps({
