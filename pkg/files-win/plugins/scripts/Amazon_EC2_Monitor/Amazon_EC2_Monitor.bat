211bdc54e91d84481bb57cc7497346783bb66d51
unknown <asalim@pso-asalim.uptimesoftware.com>
2014-03-17 11:23:24 -0400

    fixed relative path

--- pkg/files-win/plugins/scripts/Amazon_EC2_Monitor/Amazon_EC2_Monitor.bat
+++ pkg/files-win/plugins/scripts/Amazon_EC2_Monitor/Amazon_EC2_Monitor.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-set PHPDIR=..\..\apache\php\
+set PHPDIR=..\..\..\apache\php\
 set INSTANCES="%CD%\instances.txt"
 python Amazon_EC2_Monitor.py
-if errorlevel 0 ..\addsystem.exe %INSTANCES% >> addsystem.log
+if errorlevel 0 ..\..\addsystem.exe %INSTANCES% >> addsystem.log
 if errorlevel 0	"%PHPDIR%\php.exe" Amazon_EC2_Monitor_Update_Host_Check.php >> updatehostcheck.log
 exit /b %errorlevel%
