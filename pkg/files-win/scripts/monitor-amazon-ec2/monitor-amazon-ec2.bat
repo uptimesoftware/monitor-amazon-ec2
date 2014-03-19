f08ed8e21a75cc90607e99996c1eeb91aac41af2
a-salim <salim.abdullah@gmail.com>
2014-03-19 12:24:34 -0400

    fixed path

--- pkg/files-win/scripts/monitor-amazon-ec2/monitor-amazon-ec2.bat
+++ pkg/files-win/scripts/monitor-amazon-ec2/monitor-amazon-ec2.bat
@@ -3,5 +3,5 @@ set PHPDIR=..\..\apache\php\
 set INSTANCES="%CD%\instances.txt"
 python ..\..\plugins\scripts\monitor-amazon-ec2\monitor-amazon-ec2.py
 if errorlevel 0 ..\addsystem.exe %INSTANCES% >> addsystem.log
-if errorlevel 0	"%PHPDIR%\php.exe" ..\..\plugins\scripts\monitor-amazon-ec2-update-host-check.php >> updatehostcheck.log
+if errorlevel 0	"%PHPDIR%\php.exe" ..\..\plugins\scripts\monitor-amazon-ec2\monitor-amazon-ec2-update-host-check.php >> updatehostcheck.log
 exit /b %errorlevel%
